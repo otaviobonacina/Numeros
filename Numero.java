0d0a61d89a51546b07e2bcf0524101430ae850e2
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-06-06 18:39:33 +0000

    p2 numero

---
+++ Numero.java
@@ -0,0 +1,10 @@
+public class Numero {
+    public static void main(String[]args) {
+        for  (int n=38; n<1000;n++){
+            System.out.print(","+n);
+        }
+    }
+}
+            
+        
+    
